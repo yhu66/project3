f6553190fb574b9b72cfc9df0daf5b714f889563
yhu66 <54507360+yhu66@users.noreply.github.com>
2019-11-16 21:37:34 -0600

    Update map.js

--- map.js
+++ map.js
@@ -2,4 +2,7 @@ function initMap() {
             var hankow = {lat: 30.576, lng: 114.298};
             var map = new google.maps.Map(document.getElementById('map'), {zoom: 8, center: hankow});
             var marker = new google.maps.Marker({position: hankow, map: map});
+            
+            
 }             
+
