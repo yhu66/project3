d4234342e942f8f632568780b6b8105d8c23de42
yhu66 <54507360+yhu66@users.noreply.github.com>
2019-11-16 21:01:54 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 30.576, lng: 114.298},
+              zoom: 8
         });
       }
